be671fe00151c87a1204caf5ac4d742bfe969cd2
adrigs <devilcaos92@gmail.com>
2016-03-28 22:15:09 +0200

    Primera version (no jugable)

---
+++ DarkNight/Assets/Standard Assets/Scripts/Movimiento.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movimiento : MonoBehaviour {
+
+    public float velocidad = 6f;
+
+    Rigidbody playerRigibody;
+    Vector3 movimiento;
+    int floorMask;
+
+    void Awake()
+    {
+        floorMask = LayerMask.GetMask("Floor");
+        playerRigibody = GetComponent<Rigidbody>();
+    }
+	
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Mover(h, v);
+        Girar();
+	}
+
+    private void Mover(float h, float v)
+    {
+        movimiento.Set(h, 0f, v);
+        movimiento = movimiento.normalized * velocidad * Time.deltaTime;
+
+        playerRigibody.MovePosition(transform.position + movimiento);
+    }
+
+    private void Girar()
+    {
+
+
+    }
+}
---
+++ DarkNight/Assets/Standard Assets/Scripts/Pausa.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pausa : MonoBehaviour {
+
+    private bool juegoPausado = false;
+    private bool mostrarMenu = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+       if(Input.GetKeyDown(KeyCode.Escape)) pausar();
+	}
+
+    private void pausar()
+    {
+        juegoPausado = !juegoPausado;
+
+        if (juegoPausado)
+        {
+
+            Time.timeScale = 0;
+           // GameObject.Find("Main Camera").GetComponent(MouseLook).enabled = false;
+           // GameObject.Find("First Person Controller").GetComponent(MouseLook).enabled = false;
+            mostrarMenu = true;
+        }
+        else
+        {
+            Time.timeScale = 1;
+          //  GameObject.Find("Main Camera").GetComponent(MouseLook).enabled = true;
+          //  GameObject.Find("First Person Controller").GetComponent(MouseLook).enabled = true;
+            mostrarMenu = false;
+        }
+
+        if (mostrarMenu) GameObject.Find("MenuPausa").GetComponent<GUITexture>().enabled = true;
+        else GameObject.Find("MenuPausa").GetComponent<GUITexture>().enabled = false;
+
+    }
+}
